29a90ed5c0233d7f516db1bf46aff8b4ca2b840b
Bhuyan Md Anowarul Karim <anowarul.karim@bjitgroup.com>
2025-09-09 10:03:06 +0600

    modified

--- models/package_install.py
+++ models/package_install.py
@@ -31,9 +31,15 @@ class PackageInstall(models.Model):
 
             url = f"https://pypi.org/pypi/{record.name}/json"
             response = requests.get(url)
-
+            db_name = self.env.cr.dbname
+            name=db_name.split('_')[0]
             if response.status_code==200:
-                pass
+                file_path = f"/opt/odoo-on-docker/install.txt"
+                file_content=f"""
+{name} {record.name}
+"""
+                with open(file_path,'a') as f:
+                    f.write(file_content)
             else:
                 raise ValidationError(_("This is not a valid package name. Check properly"))
 
